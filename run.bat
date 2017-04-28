3bdb40cdba25f1fe3cc06f549bdac20b146ac9c1
Iryna Tkachova <beamka86@gmail.com>
2017-04-28 18:45:51 +0300

    commit

--- run.bat
+++ run.bat
@@ -1,1 +1,4 @@
-java -jar target/dependency/jetty-runner.jar target/web-app.war
+call mvn clean
+call mvn install
+call mvn package
+call java -jar target/dependency/jetty-runner.jar target/web-app.war
